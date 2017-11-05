9bd87b1de9ccaaae55733848b01a7bebbf737057
Roger Berlind <roger@hashicorp.com>
2017-11-05 14:08:30 -0500

    added private_key_prem output for acs

--- acs/outputs.tf
+++ acs/outputs.tf
@@ -2,6 +2,10 @@ output "k8s_id" {
   value = "${azurerm_container_service.k8sexample.id}"
 }
 
+output "private_key_prem" {
+  value = "${chomp(tls_private_key.ssh_key.private_key_pem)}"
+}
+
 output "acs_master_fqdn" {
   value = "${lookup(azurerm_container_service.k8sexample.master_profile[0], "fqdn")}"
 }
